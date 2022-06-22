b7a323d1f855b2dbd71229c735f5291b565f37d7
wangyu <1293527821@qq.com>
2022-06-22 16:55:01 +0800

    secondcommit

--- src/main/java/com/woniu/test/GitTest.java
+++ src/main/java/com/woniu/test/GitTest.java
@@ -3,5 +3,6 @@ package com.woniu.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("first gitadd");
+        System.out.println("second gitadd");
     }
 }
